667c8621eca4283c9bf97c0204fbefb5888a01e2
Himanshu Borah <borahhimanshu3@gmail.com>
2025-02-14 10:32:31 +0530

    fixed: single post template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./*.html"], // Update this to match your project's file structure
+  content: ["./single-post.html"], // Update this to match your project's file structure
   theme: {
     extend: {
       colors: {
